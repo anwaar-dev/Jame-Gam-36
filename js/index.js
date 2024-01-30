49d8b17448ef2a66c84a2aa568e850c869293f62
anwaar-dev <anwaarullah2005@gmail.com>
2024-01-30 16:46:46 +0500

    Player can Move and jump

--- js/index.js
+++ js/index.js
@@ -1,2 +1,42 @@
 kaboom();
 
+// CONSTANTS
+setGravity(1600)
+const SPEED = 300;
+
+
+// load a sprite from an image
+loadSprite("player", "assets/characters/player.png")
+
+// add Player to screen
+const Player = add([
+    sprite("player"),
+    pos(80, 40),
+    area(),
+    body(),
+])
+
+// add platform
+add([
+    rect(width(), 58),
+    pos(0, height() - 58),
+    outline(2),
+    area(),
+    body({ isStatic: true }),
+    color(127, 200, 255),
+])
+
+// move by SPEED pixels per frame every frame when left arrow key is being held down
+onKeyDown("a", () => {
+    Player.move(-SPEED, 0)
+})
+onKeyDown("d", () => {
+    Player.move(SPEED, 0)
+})
+
+// .jump() when "space" key is pressed
+onKeyPress("space", () => {
+    if (Player.isGrounded()) {
+        Player.jump();
+    }
+});
