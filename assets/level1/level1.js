95c180376e6a15a8844bf8427cbc088d981009f8
Haris Khan <hariskhan56.dev@gmail.com>
2024-02-01 17:55:05 +0500

    level1 plateforms

---
+++ assets/level1/level1.js
@@ -0,0 +1,73 @@
+
+export const level1Layout = [
+	[
+		"                          $",
+		"                          $",
+		"                          $",
+		"                          $",
+		"                          $",
+		"           $$         =   $",
+		"  %      ====         =   $",
+		"                      =   $",
+		"                      =    ",
+		"       ^^      = >    =   @",
+		"===========================",
+	],
+
+]
+
+// define what each symbol means in the level graph
+const levelConf = {
+	width: 100,
+	height: 100,
+	"=": () => [
+		sprite("plat1"),
+		area(),
+		solid(),
+	],
+	"$": () => [
+		sprite("coin"),
+		area(),
+		pos(0, -9),
+		origin("bot"),
+		"coin",
+	],
+	"%": () => [
+		sprite("prize"),
+		area(),
+		solid(),
+		origin("bot"),
+		"prize",
+	],
+	"^": () => [
+		sprite("spike"),
+		area(),
+		solid(),
+		origin("bot"),
+		"danger",
+	],
+	"#": () => [
+		sprite("apple"),
+		area(),
+		origin("bot"),
+		body(),
+		"apple",
+	],
+	">": () => [
+		sprite("ghosty"),
+		area(),
+		origin("bot"),
+		body(),
+		patrol(),
+		"enemy",
+	],
+	"@": () => [
+		sprite("portal"),
+		area({ scale: 0.5, }),
+		origin("bot"),
+		pos(0, -12),
+		"portal",
+	],
+}
+
+const game_Level = addLevel(map, level_config)
